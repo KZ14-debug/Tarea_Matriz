91a6ace1a5dc57edc0e6c5c6fb31ba621278506a
KZ14-debug <kendallzamora2@gmail.com>
2026-05-11 15:13:44 -0600

    Add files via upload

--- Tarea05-ED/Matriz/Matriz/Matrix.h
+++ Tarea05-ED/Matriz/Matriz/Matrix.h
@@ -74,6 +74,36 @@ public:
 
 	}
 
+	E getValue(int fila, int columna)
+	{
+		if (fila < 0 || fila >= filas)
+		{
+			runtime_error("Valor no valido.");
+		}
+
+		if (columna < 0 || columna >= columnas)
+		{
+			runtime_error("Valor no valido.");
+		}
+
+		return matrix[fila][columna];
+	}
+
+	void setValue(int fila, int columna, E value)
+	{
+		if (fila < 0 || fila >= filas)
+		{
+			runtime_error("Valor no valido.");
+		}
+
+		if (columna < 0 || columna >= columnas)
+		{
+			runtime_error("Valor no valido.");
+		}
+
+		matrix[fila][columna] = value;
+	}
+
 	int getFilas()
 	{
 		return fila;
@@ -83,6 +113,227 @@ public:
 	{
 		return columna;
 	}
+
+
+	void setAll(E value)
+	{
+		for (int i = 0; i < fila; i++)
+		{
+			for (int j = 0; j < columna; j++)
+			{
+				matrix[i][j] = value;
+			}
+		}
+	}
+
+	void transpose()
+	{
+		E** mTranspuesta = new E * [columna];
+
+		for (int i = 0; i < columna; i++)
+		{
+			mTranspuesta[i] = new E[fila];
+		}
+
+
+		for (int i = 0; i < fila; i++)
+		{
+			for (int j = 0; j < columna; j++)
+			{
+				mTranspuesta[j][i] = matrix[i][j];
+			}
+		}
+
+
+		for (int i = 0; i < fila; i++)
+		{
+			delete[] matrix[i];
+		}
+
+		delete[] matrix;
+
+
+		matrix = mTranspuesta;
+	
+
+		int temp = fila;
+		fila = columna;
+		columna = temp;
+	}
+
+	void addRow(E value)
+	{
+
+		E** mNueva= new E * [fila + 1];
+
+		for (int i = 0; i < fila + 1; i++)
+		{
+			mNueva[i] = new E[columa];
+		}
+
+
+		for (int i = 0; i < fila; i++)
+		{
+			for (int j = 0; j < columna; j++)
+			{
+				mNueva[i][j] = matrix[i][j];
+			}
+		}
+
+
+		for (int j = 0; j < columna; j++)
+		{
+			nueva[fila][j] = value;
+		}
+
+
+		for (int i = 0; i < fila; i++)
+		{
+			delete[] matrix[i];
+		}
+
+		delete[] matrix;
+
+
+		matrix = mNueva;
+
+
+		fila++;
+
+	}
+
+	void addColumn(E value)
+	{
+
+		E** mNueva = new E * [fila];
+
+		for (int i = 0; i < fila; i++)
+		{
+			mNueva[i] = new E[columna + 1];
+		}
+
+		
+		for (int i = 0; i < fila; i++)
+		{
+			for (int j = 0; j < columna; j++)
+			{
+				mNueva[i][j] = matrix[i][j];
+			}
+		}
+
+		
+		for (int i = 0; i < fila; i++)
+		{
+			mNueva[i][columna] = value;
+		}
+
+		
+		for (int i = 0; i < fila; i++)
+		{
+			delete[] matrix[i];
+		}
+
+		delete[] matrix;
+
+		
+		matrix = mNueva;
+
+		columna++;
+
+	}
+
+	void removeRow(int row)
+	{
+		if (row < 0 || row >= fila)
+		{
+			throw runtime_error("fila no valida.");
+		}
+
+
+		E** mNueva = new E * [fila - 1];
+
+		for (int i = 0; i < fila - 1; i++)
+		{
+			mNueva[i] = new E[columna];
+		}
+
+		int filaNueva = 0;
+
+		for (int i = 0, i < fila; i++)
+		{
+			if (i != row)
+			{
+				for (int j = 0; j < columna; j++)
+				{
+					mNueva[filaNueva][j] = matrix[i][j];
+				}
+			}
+			
+			filaNueva++;
+		}
+
+
+		for (int i = 0; i < fila; i++)
+		{
+			delete[] matrix[i];
+		}
+
+		delete[] matrix;
+
+
+		matrix = mNueva;
+
+		fila--;
+	}
+
+	void removeColumn(int col)
+	{
+
+		if (col < 0 || col >= columna)
+		{
+			throw runtime_error("columna no valida.");
+		}
+
+
+		E** mNueva = new E * [fila];
+
+		for (int i = 0; i < fila; i++)
+		{
+			mNueva[i] = new E[columna - 1];
+		}
+
+
+		for (int i = 0; i < fila; i++)
+		{
+
+			int columnaNueva = 0;
+
+			for (int j = 0; j < columna; j++)
+			{
+
+				if (j != col)
+				{
+					mNueva[i][columnaNueva] = matrix[i][j];
+					columnaNueva++;
+				}
+
+			}
+
+		}
+
+		for (int i = 0; i < fila; i++)
+		{
+			delete[] matrix[i];
+		}
+
+		delete[] matrix;
+
+
+		matrix = mNueva;
+		columna--;
+
+	}
+
 
 	void print()
 	{
